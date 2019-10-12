3b47101a547d7fde9649b8b9b1852c6d784ba7a3
Ofir <ofirrolev@gmail.com>
2019-10-12 15:45:43 +0300

    The player can now throw RetractorObjects that attach to the object they hit

---
+++ Assets/Scripts/RetractorAbilityManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class RetractorAbilityManager : MonoBehaviour
+{
+    public Collider2D playerCollider;
+    public RetractorManager retractorPrefab;
+    float retractorThrowPower = 2f;
+
+
+    RetractorManager retractor1 = null;
+    RetractorManager retractor2 = null;
+    float ropeLength = 1000f; // TODO temporary inital length, remove this later, and set it to the initial length after setting the retractors.
+
+    // User inputs
+    Vector3 mousePosition;
+    bool setRetractorInput = false;
+    bool useRetractorInput = false;
+
+    void UseRetractor()
+    {
+        // TODO
+    }
+
+    void SetRetractor()
+    {
+        RetractorManager createdRetractor;
+        if (retractor1 == null)
+        {
+            createdRetractor = retractor1 = Instantiate(retractorPrefab);
+        }
+        else
+        {
+            if (retractor2 == null)
+            {
+                createdRetractor = retractor2 = Instantiate(retractorPrefab);
+                Physics2D.IgnoreCollision(retractor1.GetComponent<Collider2D>(), retractor2.GetComponent<Collider2D>());
+            }
+            else
+            {
+                Destroy(retractor1.gameObject);
+                Destroy(retractor2.gameObject);
+                createdRetractor = retractor1 = Instantiate(retractorPrefab);
+                retractor2 = null;
+            }
+        }
+        createdRetractor.transform.position = transform.position;
+        Physics2D.IgnoreCollision(createdRetractor.GetComponent<Collider2D>(), playerCollider);
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        Vector3 targetLocation = ray.origin + (ray.direction * -ray.origin.z);
+        Vector3 diff = targetLocation - transform.position;
+        Vector3 force = diff.normalized * retractorThrowPower;
+        createdRetractor.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        if (Input.GetButtonDown("SetRetractor"))
+        {
+            setRetractorInput = true;
+        }
+        if (Input.GetButtonDown("UseRetractor"))
+        {
+            useRetractorInput = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (useRetractorInput)
+        {
+            useRetractorInput = false;
+            UseRetractor();
+        }
+        else if (setRetractorInput)
+        {
+            setRetractorInput = false;
+            SetRetractor();
+        }
+    }
+}
---
+++ Assets/Scripts/RetractorManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RetractorManager : MonoBehaviour
+{
+    public float maximumTimeForAttachment = 1f;
+    GameObject attachedObject = null;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("DestroyIfNoAttach", maximumTimeForAttachment);
+    }
+
+    void DestroyIfNoAttach()
+    {
+        if (attachedObject == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        AttachByCollision(collision);
+    }
+
+    private void AttachByCollision(Collision2D collision)
+    {
+        rb.simulated = false;
+        attachedObject = collision.collider.gameObject;
+        transform.SetParent(attachedObject.transform);
+        transform.position = collision.contacts[0].point;
+        Vector2 normal = collision.contacts[0].normal;
+        transform.rotation = Quaternion.FromToRotation(Vector2.up, normal);
+    }
+
+    internal bool IsAttached() => attachedObject != null;
+}
