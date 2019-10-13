df7c3699f46f1301d8223fe4e2a4eb7bdaafff0f
omri <42713743+leonidas3712@users.noreply.github.com>
2019-10-13 14:41:31 +0300

    basic foe(no shotting)

---
+++ Assets/Scripts/Enemies/Foe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foe : MonoBehaviour
+{
+    Gun gun;
+    GameObject player;
+    [SerializeField]
+    float sightRange;
+    int layerMask;
+    bool playerSighted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = ~(LayerMask.GetMask("enemies"));
+        player = GameObject.Find("Player");
+        gun = GetComponent<Gun>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerSighted) gun.Fire();
+        else SearchForPlayer();
+    }
+    
+    void SearchForPlayer()
+    {
+        RaycastHit2D playerSight = Physics2D.Raycast(transform.position, player.transform.position-transform.position, sightRange, layerMask);
+        if(playerSight.collider)
+        if (playerSight.collider.tag == "Player") playerSighted = true;
+    }
+}
