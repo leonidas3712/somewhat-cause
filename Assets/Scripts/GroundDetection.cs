724a31e58bf099f656de4a5b7a8d203b6151fdf1
omri <42713743+leonidas3712@users.noreply.github.com>
2019-10-13 23:31:54 +0300

    air anim

--- Assets/Scripts/GroundDetection.cs
+++ Assets/Scripts/GroundDetection.cs
@@ -7,6 +7,7 @@ public class GroundDetection : MonoBehaviour
     public static bool grounded = false;
     Vector2 hitDir;
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         grounded = true;
