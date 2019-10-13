61d3d3aea007205799a58c852835d185bdf946a7
Ofir <ofirrolev@gmail.com>
2019-10-13 23:27:35 +0300

    ruined everything

---
+++ Assets/Scripts/Enabler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Enabler : MonoBehaviour
+{
+    public GameObject gameObjectToEnable;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObjectToEnable.SetActive(true);
+    }
+}
