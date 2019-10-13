94c1c07565ec34778add8416ad9a696c0aa30b1e
Ofir <ofirrolev@gmail.com>
2019-10-13 18:03:34 +0300

    Added main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+}
