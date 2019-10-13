37f3f80b071dd13c8b497a5cad362fb8a5fb5767
Ofir <ofirrolev@gmail.com>
2019-10-13 22:52:30 +0300

    No time

---
+++ Assets/WinComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinComponent : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex+1);
+    }
+}
